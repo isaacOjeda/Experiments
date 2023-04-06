fba94dd9d08afa9d51aae04f68bfede87bcc42fc
Isaac Ojeda <isaac.ojeda@intelectix.com>
2023-04-06 10:46:02 -0600

    Uso de liberría Auth0

--- Auth0Test01/Auth0Test01.Web/Program.cs
+++ Auth0Test01/Auth0Test01.Web/Program.cs
@@ -1,7 +1,6 @@
+using Auth0.AspNetCore.Authentication;
 using Auth0Test01.Web;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 /**
 Refs:
 https://auth0.com/blog/call-protected-api-in-aspnet-core/
@@ -9,7 +8,9 @@ https://community.auth0.com/t/why-is-my-access-token-not-a-jwt-opaque-token/3102
 https://github.com/auth0-samples/auth0-aspnetcore-mvc-samples
 https://github.com/auth0-blog/call-protected-api-aspnet-core
 https://auth0.com/blog/exploring-auth0-aspnet-core-authentication-sdk/
+https://auth0.com/docs/quickstart/webapp/aspnet-core
 */
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,52 +24,70 @@ builder.Services.AddHttpClient("Api", c =>
 })
 .AddHttpMessageHandler<TokenHandler>();
 
-builder.Services.AddAuthentication(options =>
+
+builder.Services.AddAuth0WebAppAuthentication(options =>
 {
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-})
-.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
-{
-    options.Cookie.Name = ".ClientWebAppAuth";
-})
-.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
-{
-    options.Authority = builder.Configuration["Auth0:Domain"];
+    options.Domain = builder.Configuration["Auth0:Domain"];
     options.ClientId = builder.Configuration["Auth0:ClientId"];
     options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
-    options.ResponseType = OpenIdConnectResponseType.Code;
-    options.TokenValidationParameters.NameClaimType = "name";
-    options.SaveTokens = true;
+    options.Scope = "openid profile email";
 
-    options.Events.OnRedirectToIdentityProviderForSignOut = (context) =>
-    {
-        var logoutUri = $"{builder.Configuration["Auth0:Domain"]}/v2/logout?client_id={builder.Configuration["Auth0:ClientId"]}";
-
-        var postLogoutUri = context.Properties.RedirectUri;
-        if (!string.IsNullOrEmpty(postLogoutUri))
-        {
-            if (postLogoutUri.StartsWith("/"))
-            {
-                // transform to absolute
-                var request = context.Request;
-                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
-            }
-            logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
-        }
-
-        context.Response.Redirect(logoutUri);
-        context.HandleResponse();
-
-        return Task.CompletedTask;
-    };
-    options.Events.OnRedirectToIdentityProvider = context =>
-    {
-        context.ProtocolMessage.SetParameter("audience", builder.Configuration["Auth0:Audience"]);
-
-        return Task.CompletedTask;
-    };
+})
+.WithAccessToken(options =>
+{
+    options.Audience = builder.Configuration["Auth0:Audience"];
 });
+builder.Services.Configure<CookieAuthenticationOptions>(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+{
+    options.LoginPath = "/Login";
+});
+
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+//})
+//.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+//{
+//    options.Cookie.Name = ".ClientWebAppAuth";
+//})
+//.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+//{
+//    options.Authority = builder.Configuration["Auth0:Domain"];
+//    options.ClientId = builder.Configuration["Auth0:ClientId"];
+//    options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
+//    options.ResponseType = OpenIdConnectResponseType.Code;
+//    options.TokenValidationParameters.NameClaimType = "name";
+//    options.SaveTokens = true;
+
+//    options.Events.OnRedirectToIdentityProviderForSignOut = (context) =>
+//    {
+//        var logoutUri = $"{builder.Configuration["Auth0:Domain"]}/v2/logout?client_id={builder.Configuration["Auth0:ClientId"]}";
+
+//        var postLogoutUri = context.Properties.RedirectUri;
+//        if (!string.IsNullOrEmpty(postLogoutUri))
+//        {
+//            if (postLogoutUri.StartsWith("/"))
+//            {
+//                // transform to absolute
+//                var request = context.Request;
+//                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+//            }
+//            logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+//        }
+
+//        context.Response.Redirect(logoutUri);
+//        context.HandleResponse();
+
+//        return Task.CompletedTask;
+//    };
+//    options.Events.OnRedirectToIdentityProvider = context =>
+//    {
+//        context.ProtocolMessage.SetParameter("audience", builder.Configuration["Auth0:Audience"]);
+
+//        return Task.CompletedTask;
+//    };
+//});
 
 
 var app = builder.Build();
@@ -86,6 +105,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages()
